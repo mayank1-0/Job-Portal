8741e8ac048e6118a7fdf9b72e34e06fc8fa04de
mayank034 <mayank@studiographene.com>
2021-05-11 12:19:33 +0530

    ad

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
-const config = require('../config/config.json');
+const config = require('../config.json');
 
 module.exports = (req, res, next) => {
   try {
-    console.log(req.headers.authorization);
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, config.jwtSecret);
-    console.log(decodedToken);
+    console.log(req.headers.authorization);   //displays the authorization part i.e Type and Token
+    const token = req.headers.authorization.split(' ')[1];  //splits Type and Token by ' ' and [1] here represents index 1.
+    const decodedToken = jwt.verify(token, config.jwtSecret); //encodes token again(which gives the decoded version) with jwtSecret. Also, checks if token is expired or not.
+    console.log(decodedToken);  //gives the decoded token.
     // const userId = decodedToken.userId;
     next();
     // if (req.body.userId && req.body.userId !== userId) {
